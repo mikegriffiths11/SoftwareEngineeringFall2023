182573d3f4242ec0e7bb627fbb9b2ab6da579cfe
Michael E. Griffiths <Michael.Griffiths@svu.edu>
2023-11-06 19:52:46 -0500

    First commit

---
+++ testapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db import models
+
+class Term(models.Model):
+    school_year = models.IntegerField()
+    term_id = models.IntegerField()
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def year_term(self):
+        return str(self.school_year) + str(self.term_id)
+
+
+
+
+# Create your models here.
