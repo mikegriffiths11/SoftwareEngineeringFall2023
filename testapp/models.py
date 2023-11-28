885348d0b81f71ba36bc60de6a9fa06fad496238
Jocelyn Stewart <jocelyn.stewart@svu.edu>
2023-11-28 13:24:41 -0500

    created 2 django classes, and a random testing one just to chnge code

--- testapp/models.py
+++ testapp/models.py
@@ -1,19 +1,59 @@
 from django.db import models
-from django.db import models
 
-class Term(models.Model):
-    school_year = models.IntegerField()
-    term_id = models.IntegerField()
-    start_date = models.DateField(null=True, blank=True)
-    end_date = models.DateField(null=True, blank=True)
+class User(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
-    def year_term(self):
-        return str(self.school_year) + str(self.term_id)
+    def full_name(self):
+        return str(self.first_name) + ' ' + str(self.last_name)
 
 
 
 
 # Create your models here.
+#need to make two classes each
+
+#I'm going to mess around a bit
+
+# item
+#login (user id)
+#food
+#beverage
+#utility
+#shipping
+
+#order
+
+class Item(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    type = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Food(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    type = models.TextField()
+
+
+
+
+#trying, in class Thurs Nov 16
+
+class MinutesMixIn(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LocationsofItems(MinutesMixIn):
+    storenumber = models.TextField()
+    room = models.TextField()
+    shelf = models.IntegerField()
+
+class NewMessingAround(models.Model):
+    testing = models.TextField()
+
